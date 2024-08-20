f53b32ba851ab4b5a5db53fa215098b2893b71a6
Hanna Lakus <annalakus@ukr.net>
2024-08-20 23:50:06 +0100

    load more fail

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,21 +2,33 @@ import { useEffect, useState } from "react";
 import ListOfFilms from "../ListOfFilms/ListOfFilms";
 import fetchData from "../../FetchData";
 import { useLocation } from "react-router-dom";
+// import LoadMore from "../LoadMore/LoadMore";
 
 export default function MovieList() {
   const [listFilms, setListFilms] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  // const [page, setPage] = useState(2);
+  // const [totalPages, setTotalPages] = useState(1);
   const location = useLocation();
 
   const endPoint = "trending/movie/day";
+
+  // function addPage() {
+  //   setPage(page + 1);
+  // }
 
   useEffect(() => {
     const getFilmsList = async () => {
       try {
         setLoading(true);
+        setError(false);
+
         const respons = await fetchData(1, "", endPoint);
-        setListFilms(respons.results);
+
+        // setTotalPages(respons.total_pages);
+
+        setListFilms([...respons.results]);
       } catch {
         setError(true);
         (err) => console.error(err);
@@ -37,6 +49,7 @@ export default function MovieList() {
       {listFilms.length > 0 && (
         <ListOfFilms list={listFilms} state={location.state} />
       )}
+      {/* {page < totalPages && <LoadMore onClick={addPage} />} */}
     </>
   );
 }
