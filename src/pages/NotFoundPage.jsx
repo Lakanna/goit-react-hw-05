65c12735c0a526af8ecb097a171bbba5f1ba9251
Hanna Lakus <annalakus@ukr.net>
2024-08-20 21:55:14 +0100

    add link to not found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
   return (
     <main>
-      <div>Not found page</div>
+      <h2>Sorry, this page is not found</h2>
+      <Link to="/">Go Home</Link>
     </main>
   );
 }
