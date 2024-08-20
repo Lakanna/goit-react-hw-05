0a63aba5e800fcb3457177d5f97dacb33478a9e3
Hanna Lakus <annalakus@ukr.net>
2024-08-20 17:41:22 +0100

    add toast

--- src/components/SearchForm/SearhForm.jsx
+++ src/components/SearchForm/SearhForm.jsx
@@ -1,12 +1,26 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import css from "./SearhForm.module.css";
 
 export default function SearhForm({ onSubmit }) {
+  const notify = () =>
+    toast.warn("😈 Please, input value for search", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
   function handleSubmit(e) {
     e.preventDefault();
     const form = e.currentTarget;
     const value = form.elements.searchingFilms.value.trim();
 
     if (value === "") {
+      notify();
       console.log("Please, input value for search");
       return;
     }
@@ -23,6 +37,7 @@ export default function SearhForm({ onSubmit }) {
         placeholder="Movies title"
       />
       <button type="submit">Search</button>
+      <ToastContainer />
     </form>
   );
 }
